5b33d542ed2b64dea75e35effe8f95e0951bfd6d
gercovv <sasamrfn@gmail.com>
2022-06-16 01:25:37 +0300

    fix: edit page button url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Gercov/OldBoty-Docs',
+            'https://github.com/Gercov/OldBoty-Docs/blob/master',
           sidebarCollapsed: false
         },
         theme: {
