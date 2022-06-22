d581d88336ff747f98a52c200c26ae2dad6f0333
gercovv <sasamrfn@gmail.com>
2022-06-22 18:57:09 +0300

    feat: innovation page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,7 +59,7 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'install',
+            docId: 'innovations',
             position: 'left',
             label: 'Документация',
           },
